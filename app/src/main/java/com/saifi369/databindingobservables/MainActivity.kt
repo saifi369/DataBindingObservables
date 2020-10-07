7ed5d096c6b64175ef6d836f0f4b004d93c63d8e
saifi369 <chsaif369@gmail.com>
2020-10-07 22:02:47 +0500

    Observable Collections, ObservableArrayMap & ObservableArrayList

--- app/src/main/java/com/saifi369/databindingobservables/MainActivity.kt
+++ app/src/main/java/com/saifi369/databindingobservables/MainActivity.kt
@@ -2,29 +2,36 @@ package com.saifi369.databindingobservables
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ObservableField
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableArrayMap
 import com.saifi369.databindingobservables.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var person: ObservableField<PrimitiveObservablePerson> =
-        ObservableField(PrimitiveObservablePerson("Ali", 35))
+    val listData: ObservableArrayList<String> = ObservableArrayList()
+    val mapData: ObservableArrayMap<String, Any> = ObservableArrayMap()
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.person = person
+        listData.add("Ali")
+        listData.add("Usman")
+        listData.add("Hamza")
+
+        mapData.put("name", "Ali")
+        mapData.put("age", 35)
+
+        binding.data = mapData
         binding.handler = this
     }
 
     fun onClick(view: View) {
-        person.set(PrimitiveObservablePerson("AA", 100))
-//        person.get()?.name?.set("Usman")
-        Toast.makeText(this, "Person age is ${person.get()?.age?.get()}", Toast.LENGTH_SHORT).show()
+        mapData.remove("name")
+        mapData.remove("age")
     }
 }
